8338bf14010d36394deec30c59879d80e29ef120
Felipe Samas <felipesamas.dev@gmail.com>
2023-08-06 17:57:24 -0300

    bad soluction 2

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -46,10 +46,10 @@ public class Reservation {
 		Date now = new Date();
 		// METODO RUIM
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Error in reservation: Check-out date must be after check-in date";
-		}
-		if (!checkOut.after(checkIn)) {
 			return "Error in reservation: Reservation dates for update must be future dates";
+		}
+		if (!checkOut.after(checkIn)) { 
+			return "Error in reservation: Check-out date must be after check-in date";
 		}
 
 		this.checkIn = checkIn;
